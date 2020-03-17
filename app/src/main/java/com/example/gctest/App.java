9b792b42c87be6e1f46f15f6137a854269df431e
lixiangyan <lixiangyan@meizu.com>
2020-03-17 11:43:40 +0800

    change leakcanary to 1.6.3 version, but not cause leak check result

---
+++ app/src/main/java/com/example/gctest/App.java
@@ -0,0 +1,11 @@
+package com.example.gctest;
+
+import android.app.Application;
+import com.squareup.leakcanary.LeakCanary;
+public class App extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        LeakCanary.install(this);
+    }
+}
