960c357e7fe84b72c7bbdbc8a6cb0086ba607348
lixiangyan <lixiangyan@meizu.com>
2020-04-17 10:12:21 +0800

    add Application

---
+++ app/src/main/java/com/example/gctest/App.java
@@ -0,0 +1,14 @@
+package com.example.gctest;
+
+import android.app.Application;
+import android.util.Log;
+public class App extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+    
+
+}
